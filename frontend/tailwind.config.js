236d97546a356ab585669a1ae26394c895db829b
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2022-12-21 16:13:34 +0100

    Added custom color theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,4 +5,21 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        "nomad-light": {
+          primary: "#E70B67",
+          secondary: "#3F3D56",
+          accent: "#f1cfdd",
+          neutral: "#2F4858",
+          "base-100": "#F2f2f2",
+          info: "#FC2F20",
+          success: "#B13990",
+          warning: "#6D4E97",
+          error: "#2f2e41",
+        },
+      },
+    ],
+  },
 };
