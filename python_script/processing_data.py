3b0e1e2083643a1d119f80f4b2f462f1d7980c30
Rapelli Riccardo <riky.rapelli@gmail.com>
2022-12-28 17:43:59 +0100

    Update processing_data.py, delete test.py

--- python_script/processing_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-csv_path = 'https://raw.githubusercontent.com/FedericoFornone/Laboratorio_Integrato/fintech/DataSet/turismo_da_estero_in_italia.csv'
-
-df = pd.read_csv(csv_path)
-
-print(df)
