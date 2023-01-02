e03e9fcf74f2897b21a3d2fb7ea18806a2800024
Michelito00 <micky.aliverti@gmail.com>
2023-01-02 16:08:33 +0100

    Add Navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgChartsModule } from 'ng2-charts';
 
+import { StatsComponent } from "./pages/stats/stats.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, StatsComponent, NavbarComponent],
   imports: [
     HttpClientModule,
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
