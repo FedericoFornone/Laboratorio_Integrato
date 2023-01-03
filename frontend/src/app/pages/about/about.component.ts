b6d8bf48b0b7fec2eebe5dea4972f8f2f7119e68
lunadiatto <luna.diatto@gmail.com>
2023-01-03 11:44:25 +0100

    added avatar component in the about page

--- frontend/src/app/pages/about/about.component.ts
+++ frontend/src/app/pages/about/about.component.ts
@@ -5,5 +5,49 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
 })
 export class AboutComponent {
-
+  /* name = 'luna';
+  role = 'dev';
+  image = './././assets/about/team/lunadiatto.webp'; */
+  team = [
+    {
+      name: 'Federico Fornone',
+      role: 'Team Leader, Backend Developer',
+      image: './././assets/about/team/riccardorapizzi.webp' /* FIXME: */,
+    },
+    {
+      name: 'Alessia Rizzo',
+      role: 'Backend Developer',
+      image: './././assets/about/team/riccardorapizzi.webp',
+    },
+    {
+      name: 'Daniele Budano',
+      role: 'Backend Developer',
+      image: './././assets/about/team/danielebudano.webp',
+    },
+    {
+      name: 'Riccardo Rapizzi',
+      role: 'Fintech Developer',
+      image: './././assets/about/team/riccardorapizzi.webp',
+    },
+    {
+      name: 'Luca Mainardi',
+      role: 'Fintech Developer',
+      image: './././assets/about/team/lucamainardi.webp',
+    },
+    {
+      name: 'Jacopo Trompeo',
+      role: 'Web Developer',
+      image: './././assets/about/team/jacopotrompeo.webp',
+    },
+    {
+      name: 'Luna Diatto',
+      role: 'Web Developer',
+      image: './././assets/about/team/lunadiatto.webp',
+    },
+    {
+      name: 'Michele Aliverti',
+      role: 'Web Developer',
+      image: './././assets/about/team/michelealiverti.webp',
+    },
+  ];
 }
