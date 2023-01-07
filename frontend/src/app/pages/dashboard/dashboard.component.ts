be6bdf2e43c7253164c12d6c84146362d68bc78e
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2023-01-07 21:54:12 +0100

    Number of slides is no longer dynamic

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,32 +1,18 @@
-import {
-  Component,
-  OnInit,
-  ViewChildren,
-  AfterViewInit,
-  QueryList,
-  ElementRef,
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-stats',
   templateUrl: './dashboard.component.html',
 })
-export class StatsComponent implements OnInit, AfterViewInit {
-  @ViewChildren('slide') slides!: QueryList<ElementRef>;
+export class StatsComponent implements OnInit {
   currentSlide: number = 0;
-  numberOfSlides!: number;
+  numberOfSlides: number = 3;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.data.subscribe((data) => {});
-  }
-
-  ngAfterViewInit() {
-    setTimeout(() => {
-      this.numberOfSlides = this.slides.length;
-    }, 0);
   }
 
   previousSlide() {
