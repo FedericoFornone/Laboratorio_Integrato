ff5e993c44bc56048627315afd424e338e49f97f
lunadiatto <luna.diatto@gmail.com>
2023-01-11 19:33:33 +0100

    content of the app

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
-
 import { HomeComponent } from './pages/home/home.component';
 import { StatsComponent } from './pages/stats/stats.component';
-
 import { StatsResolver } from './resolvers/stats.resolver';
 
 const routes: Routes = [
