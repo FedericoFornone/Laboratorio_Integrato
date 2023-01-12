2a07fbe060bfb60db7971e02d88588b54ef35113
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2023-01-12 15:00:41 +0100

    Added api call

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -10,34 +10,6 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getStats(): Observable<any> {
-    return new Observable((observer) => {
-      observer.next({
-        lineChartData: {
-          labels: [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-          ],
-          datasets: [
-            {
-              data: [65, 59, 80, 81, 56, 55, 40],
-              label: 'Series A',
-              fill: true,
-              tension: 0.5,
-              borderColor: 'black',
-              backgroundColor: 'rgba(255,0,0,0.3)',
-            },
-          ],
-        },
-        lineChartOptions: {
-          responsive: false,
-        },
-        lineChartLegend: true,
-      });
-    });
+    return this.http.get('localhost:8080/statistics');
   }
 }
