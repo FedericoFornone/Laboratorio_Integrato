4a5b354e2e518d464aa619b159c00a33127017c4
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2023-01-13 10:06:43 +0100

    Moved from local json to api endpoint

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -30,74 +30,79 @@ export class ApiService {
     infrastructure: string = '',
     residenceCountry: string = ''
   ): Observable<any> {
-    return this.http.get('assets/abruzzo.json').pipe(
-      map((data: any) => {
-        const datasets = [];
+    return this.http
+      .get('http://18.102.24.178:7790/statistics?region=' + region)
+      .pipe(
+        map((data: any) => {
+          const datasets = [];
 
-        if (infrastructure === '' && residenceCountry === '') {
-          datasets.push({
-            label: 'Esteri in altre strutture',
-            data: this.getArrivals(data, region, 'Paesi esteri', 'OTHER'),
-            backgroundColor: '#0BE7A3',
-            borderRadius: 5,
-          });
-          datasets.push({
-            label: 'Esteri in hotel',
-            data: this.getArrivals(data, region, 'Paesi esteri', 'HOTELLIKE'),
-            backgroundColor: '#F2B705',
-            borderRadius: 5,
-          });
-          datasets.push({
-            label: 'Italiani in altre strutture',
-            data: this.getArrivals(data, region, 'Italia', 'OTHER'),
-            backgroundColor: '#0B7EE7',
-            borderRadius: 5,
-          });
-          datasets.push({
-            label: 'Italiani in hotel',
-            data: this.getArrivals(data, region, 'Italia', 'HOTELLIKE'),
-            backgroundColor: '#E70B67',
-            borderRadius: 5,
-          });
-        }
+          if (infrastructure === '' && residenceCountry === '') {
+            datasets.push({
+              label: 'Esteri in altre strutture',
+              data: this.getArrivals(data, region, 'Paesi esteri', 'OTHER'),
+              backgroundColor: '#0BE7A3',
+              borderRadius: 5,
+            });
+            datasets.push({
+              label: 'Esteri in hotel',
+              data: this.getArrivals(data, region, 'Paesi esteri', 'HOTELLIKE'),
+              backgroundColor: '#F2B705',
+              borderRadius: 5,
+            });
+            datasets.push({
+              label: 'Italiani in altre strutture',
+              data: this.getArrivals(data, region, 'Italia', 'OTHER'),
+              backgroundColor: '#0B7EE7',
+              borderRadius: 5,
+            });
+            datasets.push({
+              label: 'Italiani in hotel',
+              data: this.getArrivals(data, region, 'Italia', 'HOTELLIKE'),
+              backgroundColor: '#E70B67',
+              borderRadius: 5,
+            });
+          }
 
-        if (infrastructure === 'HOTELLIKE' && residenceCountry === 'Italia') {
-          datasets.push({
-            label: 'Italiani in hotel',
-            data: this.getArrivals(data, region, 'Italia', 'HOTELLIKE'),
-            backgroundColor: '#E70B67',
-          });
-        }
+          if (infrastructure === 'HOTELLIKE' && residenceCountry === 'Italia') {
+            datasets.push({
+              label: 'Italiani in hotel',
+              data: this.getArrivals(data, region, 'Italia', 'HOTELLIKE'),
+              backgroundColor: '#E70B67',
+            });
+          }
 
-        if (infrastructure === 'OTHER' && residenceCountry === 'Italia') {
-          datasets.push({
-            label: 'Italiani in altre strutture',
-            data: this.getArrivals(data, region, 'Italia', 'OTHER'),
-            backgroundColor: '#0B7EE7',
-          });
-        }
+          if (infrastructure === 'OTHER' && residenceCountry === 'Italia') {
+            datasets.push({
+              label: 'Italiani in altre strutture',
+              data: this.getArrivals(data, region, 'Italia', 'OTHER'),
+              backgroundColor: '#0B7EE7',
+            });
+          }
 
-        if (
-          infrastructure === 'HOTELLIKE' &&
-          residenceCountry === 'Paesi esteri'
-        ) {
-          datasets.push({
-            label: 'Esteri in hotel',
-            data: this.getArrivals(data, region, 'Paesi esteri', 'HOTELLIKE'),
-            backgroundColor: '#F2B705',
-          });
-        }
+          if (
+            infrastructure === 'HOTELLIKE' &&
+            residenceCountry === 'Paesi esteri'
+          ) {
+            datasets.push({
+              label: 'Esteri in hotel',
+              data: this.getArrivals(data, region, 'Paesi esteri', 'HOTELLIKE'),
+              backgroundColor: '#F2B705',
+            });
+          }
 
-        if (infrastructure === 'OTHER' && residenceCountry === 'Paesi esteri') {
-          datasets.push({
-            label: 'Esteri in altre strutture',
-            data: this.getArrivals(data, region, 'Paesi esteri', 'OTHER'),
-            backgroundColor: '#0BE7A3',
-          });
-        }
+          if (
+            infrastructure === 'OTHER' &&
+            residenceCountry === 'Paesi esteri'
+          ) {
+            datasets.push({
+              label: 'Esteri in altre strutture',
+              data: this.getArrivals(data, region, 'Paesi esteri', 'OTHER'),
+              backgroundColor: '#0BE7A3',
+            });
+          }
 
-        return datasets;
-      })
-    );
+          return datasets;
+        })
+      );
   }
 }
