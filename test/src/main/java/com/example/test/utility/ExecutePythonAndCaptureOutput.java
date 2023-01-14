afef104bdb959b7df5122a9f2f23ba3f4d85c254
SBDWolf <daniele.budano@edu.itspiemonte.it>
2023-01-15 00:17:24 +0100

    Added utility function to execute python script with multiple parameters

--- test/src/main/java/com/example/test/utility/ExecutePythonAndCaptureOutput.java
+++ test/src/main/java/com/example/test/utility/ExecutePythonAndCaptureOutput.java
@@ -19,4 +19,28 @@ public class ExecutePythonAndCaptureOutput {
         String result = builder.toString();
         return result;
     }
+
+    // this function should only be called if multiple parameters are needed to execute a python script
+    // the first parameter should just be the string "python"
+    // the second parameter should be the path to the python script
+    // the next parameters should be the parameters to be passed to the python script
+    // for example: ExecutePythonWithParameters("python", "path/to/file.py", "long-term")
+    public static String ExecutePythonWithParameters(String... args) throws IOException {
+        if (args[0] != "python") {
+            return "Internal error, first parameter wasn't python";
+        }
+        ProcessBuilder pb = new ProcessBuilder().command(args)
+        .redirectErrorStream(true);
+        Process p = pb.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        StringBuilder builder = new StringBuilder();
+        String line = null;
+        while (((line = reader.readLine()) != null)) {
+            builder.append(line);
+            builder.append(System.getProperty("line.separator"));
+        }
+        String result = builder.toString();
+        return result;
+    }
 }
