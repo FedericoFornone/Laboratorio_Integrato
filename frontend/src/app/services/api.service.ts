9b50052425f9ff0cf8ae285c7d929e994ed3cf24
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2023-01-17 18:17:12 +0100

    Endpoint now returns arrivals

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -11,11 +11,11 @@ export class ApiService {
 
   private assembleUrl(
     region: string,
+    date: string,
     infrastructure?: 'hotel' | 'other',
-    residenceCountry?: 'italy' | 'foreign',
-    year?: string
+    residenceCountry?: 'italy' | 'foreign'
   ) {
-    let url = 'http://18.102.24.178:7790/statistics?region=' + region;
+    let url = `http://18.102.24.178:7790/statistics?region=${region}&date=${date}`;
 
     if (infrastructure) {
       url += '&infrastructure=' + infrastructure;
@@ -25,28 +25,33 @@ export class ApiService {
       url += '&residenceCountry=' + residenceCountry;
     }
 
-    if (year) {
-      url += '&year=' + year;
-    }
-
     return url;
+  }
+
+  private getArrivals(data: ApiModel[]) {
+    const arrivals = data.map((item) => {
+      return item.Arrivals;
+    });
+
+    return arrivals;
   }
 
   getStats(
     region: string,
+    date: string = '2021',
     infrastructure?: 'hotel' | 'other',
-    residenceCountry?: 'italy' | 'foreign',
-    year?: string
+    residenceCountry?: 'italy' | 'foreign'
   ) {
     const url = this.assembleUrl(
       region,
+      date,
       infrastructure,
-      residenceCountry,
-      year
+      residenceCountry
     );
 
     return this.http.get<ApiModel[]>(url).pipe(
       map((data) => {
+        const arrivals = this.getArrivals(data);
         return data;
       })
     );
