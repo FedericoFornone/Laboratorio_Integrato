0328358b76526718c05854c61ae1d2c6855ff51d
lunadiatto <luna.diatto@gmail.com>
2023-01-22 11:30:47 +0100

    lang attribute changes on language change

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,18 +1,17 @@
 import { Component } from '@angular/core';
-import {Router} from '@angular/router';
-import {TranslateService} from '@ngx-translate/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: '../navbar/navbar.component.html',
-  styleUrls: ['../navbar/navbar.component.scss']
+  styleUrls: ['../navbar/navbar.component.scss'],
 })
 export class NavbarComponent {
-
-  constructor(public router: Router, private translate: TranslateService){}
+  constructor(public router: Router, private translate: TranslateService) {}
 
   changeLanguage(language: string): void {
     this.translate.use(language);
+    document.documentElement.lang = language;
   }
-
 }
