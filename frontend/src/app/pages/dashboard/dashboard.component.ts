8bf516c15c09029009e7cd0ed474a36ae42056ea
Michelito00 <micky.aliverti@gmail.com>
2023-01-24 09:23:43 +0100

    dark-mode

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stats',
   templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
   tutorialModalOpen = false;
