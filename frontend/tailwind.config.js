b12b2fa29ce93c0ea345bebd4f5f43918e8633b7
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2023-01-25 10:15:02 +0100

    Changed font to nunito

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require("daisyui"), require("@tailwindcss/forms")],
   daisyui: {
