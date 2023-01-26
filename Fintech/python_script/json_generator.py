92d8af7e2fcb0fc62ae87c428102a2ace2cb1f1f
RiccardoRapelli <78143302+RiccardoRapelli@users.noreply.github.com>
2023-01-26 22:15:36 +0100

    Insert all files into Fintech folder

---
+++ Fintech/python_script/json_generator.py
@@ -0,0 +1,22 @@
+import csv
+import json
+
+
+def csv_to_json(csv_path, json_path):
+    json_array = []
+
+    with open(csv_path, encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+
+        for row in csv_reader:
+            json_array.append(row)
+
+    with open(json_path, 'w', encoding='utf-8') as json_file:
+        json_string = json.dumps(json_array, indent=4)
+        json_file.write(json_string)
+
+
+# csv_path = r'../Data/final_data/turisti_stranieri_abruzzo.csv'
+csv_path = r'../Data/final_data/abruzzo.csv'
+json_path = r'../Data/final_data/abruzzo.json'
+csv_to_json(csv_path, json_path)
---
+++ Fintech/python_script/processing_data.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+
+
+if __name__ == '__main__':
+    path = '../DataSet/annuo_abruzzo.csv'
+    df = pd.read_csv(path, sep=";", low_memory=False).fillna(0)
+
+    df.drop(["ITTER107 ", "TIPO_DATO7", "CORREZ", "Correzione", "Tipologia di esercizio", "ATECO_2007", "Ateco 2007", "ISO", "Seleziona periodo", "Flag Codes", "Flags"], axis=1, inplace=True)
+
+    df.sort_values(by=["TIME", "Territorio", "Indicatori", "TIPO_ALLOGGIO2", "Paese di residenza dei clienti"], ascending=True, inplace=True)
+
+    new_df = df[df["Indicatori"] == "arrivi "].copy()
+    new_df.rename(columns={"Value": "arrivi"}, inplace=True)
+    new_df["presenze"] = df[df["Indicatori"] == "presenze"]["Value"].to_numpy()
+    new_df.drop("Indicatori", axis=1, inplace=True)
+
+    new_df["TIME"] = pd.to_datetime(new_df["TIME"], infer_datetime_format=True, format='%Y%m%d')
+
+    new_df.rename(columns={'Territorio': 'Region', 'TIPO_ALLOGGIO2': 'Infrastructure',
+                              'Paese di residenza dei clienti': 'ResidenceCountry', 'TIME': 'Date',
+                              'arrivi': 'Arrivals', 'presenze': 'Attendance'}, inplace=True)
+    try:
+        os.mkdir("..\\Data")
+    except OSError as error:
+        print(error)
+
+    new_df.to_csv("../Data/final_data/annuo_abruzzo.csv", index=False)
